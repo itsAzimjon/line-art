dee8a824da2b8bb1b47b59449ead15493b3fe696
Azimjon Ganiev <azimjonganiev4464@gmail.com>
2024-03-05 01:05:55 +0500

    Store recoded, branch table added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends Controller
         $tags = Tag::orderBy('name')->get();
         $products = Product::latest()->get();
         $categories = Category::all();
-        $branches = Branch::where('id', '<', '2')->get();
+        $branches = Branch::where('id', '>', '3')->get();
 
         return view('welcome', compact(['tags','categories', 'products', 'branches']));   
     }
